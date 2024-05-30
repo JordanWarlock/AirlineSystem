bd41b565960829e27a22816e5278c9891dcac514
gulshwrkhan <147302074+gulshwrkhan@users.noreply.github.com>
2024-05-30 17:25:36 +0500

    added booking component in homepage

--- frontend/src/Pages/BookingPage.jsx
+++ frontend/src/Pages/BookingPage.jsx
@@ -3,13 +3,19 @@ import { useNavigate } from "react-router";
 import axios from "axios";
 import "../css/BookingPage.css";
 import Searchbar from "../Components/Searchbar";
+import { TextField, Button, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, Box, MenuItem, Select, InputLabel } from '@mui/material';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const BookingPage = () => {
   const [selectedDestination, setSeletedDestination] = useState("");
   const [selectedDeparture, setSeletedDeparture] = useState("");
   const [flightType, setFlightType] = useState("OneWay");
-  const [departureDate, setDepartureDate] = useState("");
-  const [returnDate, setReturnDate] = useState("");
+  const [departureDate, setDepartureDate] = useState(null);
+  const [returnDate, setReturnDate] = useState(null);
+  const [passengerCount, setPassengerCount] = useState(1);
+  const [travelClass, setTravelClass] = useState("ECONOMY");
 
   const navigate = useNavigate();
 
@@ -25,8 +31,8 @@ const BookingPage = () => {
             departureCode: selectedDeparture["code"],
             destinationCode: selectedDestination["code"],
             depDate: departureDate,
-            passengerCount: 1,
-            cabinClass: "ECONOMY",
+            passengerCount,
+            cabinClass: travelClass,
           };
           const response = await axios.post(
             "http://localhost:5000/api/flightData/oneway",
@@ -45,8 +51,8 @@ const BookingPage = () => {
             destinationCode: selectedDestination["code"],
             depDate: departureDate,
             retDate: returnDate,
-            passengerCount: 1,
-            cabinClass: "ECONOMY",
+            passengerCount,
+            cabinClass: travelClass,
           };
           const response = await axios.post(
             "http://localhost:5000/api/flightData/return",
@@ -65,46 +71,83 @@ const BookingPage = () => {
       console.log("Fill all fields");
     }
   };
+
   return (
-    <div className="main-container">
-      <select
-        name="flight_type"
-        id="type"
-        onChange={(e) => {
-          setFlightType(e.target.value);
-        }}
-      >
-        <option value="OneWay">OneWay</option>
-        <option value="Return">Return</option>
-      </select>
-      <Searchbar
-        setSearchResult={setSeletedDeparture}
-        icon="fa-solid fa-plane"
-        placeHolder="From?"
-      />
-      <Searchbar
-        setSearchResult={setSeletedDestination}
-        icon="fa-solid fa-arrow-left"
-        placeHolder="To?"
-      />
-      <input
-        type="date"
-        onChange={(e) => {
-          setDepartureDate(e.target.value);
-        }}
-      />
-      {flightType !== "OneWay" ? (
-        <input
-          type="date"
-          onChange={(e) => {
-            setReturnDate(e.target.value);
-          }}
-        ></input>
-      ) : (
-        <></>
-      )}
-      <button onClick={() => handleSearch()}>Search</button>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <div className="main-container">
+        <h1>Book here</h1>
+          <FormControl>
+            <FormLabel className="triphere">Trip Type</FormLabel>
+            <RadioGroup row value={flightType} onChange={(e) => setFlightType(e.target.value)} className="radio-group">
+              <FormControlLabel value="OneWay" control={<Radio />} label="One Way" />
+              <FormControlLabel value="Return" control={<Radio />} label="Return" />
+            </RadioGroup>
+          </FormControl>
+          <div className="form-row">
+            <Searchbar
+              setSearchResult={setSeletedDeparture}
+              icon="fa-solid fa-plane"
+              placeHolder="From?"
+              className="text-field"
+            />
+            <Searchbar
+              setSearchResult={setSeletedDestination}
+              icon="fa-solid fa-arrow-left"
+              placeHolder="To?"
+              className="text-field"
+            />
+          </div>
+          <div className="form-row">
+            <DatePicker
+              label="Departure Date"
+              value={departureDate}
+              onChange={(newValue) => setDepartureDate(newValue)}
+              renderInput={(params) => <TextField {...params} className="date-field" />}
+            />
+            {flightType === "Return" && (
+              <DatePicker
+                label="Return Date"
+                value={returnDate}
+                onChange={(newValue) => setReturnDate(newValue)}
+                renderInput={(params) => <TextField {...params} className="date-field" />}
+              />
+            )}
+          </div>
+          <div className="form-row passenger-class-field">
+            <FormControl fullWidth>
+              <InputLabel>Passengers</InputLabel>
+              <Select
+                value={passengerCount}
+                label="Passengers"
+                onChange={(e) => setPassengerCount(e.target.value)}
+              >
+                {[...Array(10).keys()].map((num) => (
+                  <MenuItem key={num + 1} value={num + 1}>
+                    {num + 1}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel>Class</InputLabel>
+              <Select
+                value={travelClass}
+                label="Class"
+                onChange={(e) => setTravelClass(e.target.value)}
+              >
+                <MenuItem value="ECONOMY">Economy</MenuItem>
+                <MenuItem value="BUSINESS">Business</MenuItem>
+                <MenuItem value="FIRST">First</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className="search-button">
+            <Button variant="contained" color="primary" onClick={handleSearch}>
+              Search flights
+            </Button>
+          </div>
+        </div>
+    </LocalizationProvider>
   );
 };
 
--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "../css/HomePage.css";
 import Header from "../Components/Header";
 import SignUpImage from "../Pictures/signup.jpg"
+import BookingPage from "../Pages/BookingPage"
 
 const HomePage = () => {
   return (
     <div>
       <Header imageUrl={SignUpImage} />
+      <BookingPage/>
     </div>
   );
 };
