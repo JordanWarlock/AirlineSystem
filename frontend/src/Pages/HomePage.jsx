8400001ee5cf99880f93e030854777c0ff6e9a30
gulshwrkhan <147302074+gulshwrkhan@users.noreply.github.com>
2024-05-30 18:21:43 +0500

    added picture component to the homepage

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -3,12 +3,14 @@ import "../css/HomePage.css";
 import Header from "../Components/Header";
 import SignUpImage from "../Pictures/signup.jpg"
 import BookingPage from "../Pages/BookingPage"
+import MainPageComponent from "../Components/MainPageComponent";
 
 const HomePage = () => {
   return (
     <div>
       <Header imageUrl={SignUpImage} />
       <BookingPage/>
+      <MainPageComponent/>
     </div>
   );
 };
